afa2b687252b133ad81ce5728152fb22bdef7e16
pcwuditom <pcwuditom@hotmail.com>
2025-02-07 10:12:29 +0800

    xin yong ka ye wu ping tai qi zhong yi ming zhi qing ren shi shuo wei le ying dui jian guan shen zha gao

--- assets/website/js/frontend/G.js
+++ assets/website/js/frontend/G.js
@@ -123,9 +123,9 @@
 
 	    		if (name != '' && url != '')
 	    		{
-	    			$.each($('h2:contains("订阅链接")'), function(i){
-	    				var item = $('h2:contains("订阅链接")').eq(i);
-	    				if (item.text() == '订阅链接')
+	    			$.each($('h2:contains("订阅链接"), h2:contains("订阅文件链接")'), function(i){
+	    				var item = $('h2:contains("订阅链接"), h2:contains("订阅文件链接")').eq(i);
+	    				if (item.text() == '订阅链接' || item.text() == '订阅文件链接')
 	    				{
 	    					var div = $('<div style="background:#fcf8e3;border:1px solid #faf3cd;border-left: 0.5rem solid #faf3cd;padding: 0.5rem;line-height:2rem;margin-top: 1rem;color:#c09853;">以下订阅链接均来自 <b>'+name+'</b> 分享，更多免费机场订阅可 <a href="'+url+'">【跳转查看】</a></div>');
 	    					item.after(div);
